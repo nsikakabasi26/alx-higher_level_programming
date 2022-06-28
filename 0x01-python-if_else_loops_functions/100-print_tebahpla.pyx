2f6802da62553be7d0a2562172d7608489a24899
Eze <ifeanyialex2@gmail.com>
2022-06-28 12:09:36 -0700

    advancd task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.pyx
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.pyx
@@ -1,3 +1,8 @@
+
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
+for i in range(-122, -96):
+    if i % 2 == 0:
+        let = chr(i * -1)
+    else:
+        let = chr((i * -1) - 32)
+    print("{}".format(let), end="")
